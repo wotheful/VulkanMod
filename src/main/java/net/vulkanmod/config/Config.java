23e2bd322f371893c44fea7f61ba9c0ffe6d952b
Shadow <saintplayzph69@gmail.com>
2024-07-17 08:48:59 +0800

    Update Config.java

--- src/main/java/net/vulkanmod/config/Config.java
+++ src/main/java/net/vulkanmod/config/Config.java
@@ -23,6 +23,7 @@ public class Config {
 
     public boolean uniqueOpaqueLayer = true;
     public boolean entityCulling = true;
+    public boolean animations = true;
     public int imageCount = 3;
     public int device = -1;
 

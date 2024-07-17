84d03f91a75ad98aaa42b70e899ef12515d413c4
Shadow <saintplayzph69@gmail.com>
2024-07-17 08:50:42 +0800

    Update MTextureManager.java

--- src/main/java/net/vulkanmod/mixin/texture/MTextureManager.java
+++ src/main/java/net/vulkanmod/mixin/texture/MTextureManager.java
@@ -2,6 +2,7 @@ package net.vulkanmod.mixin.texture;
 
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.renderer.texture.Tickable;
+import net.vulkanmod.Initializer;
 import net.vulkanmod.render.texture.SpriteUtil;
 import net.vulkanmod.vulkan.Renderer;
 import net.vulkanmod.vulkan.device.DeviceManager;
@@ -22,7 +23,7 @@ public abstract class MTextureManager {
      */
     @Overwrite
     public void tick() {
-        if (Renderer.skipRendering)
+        if (Renderer.skipRendering || !Initializer.CONFIG.animations)
             return;
 
         //Debug D
